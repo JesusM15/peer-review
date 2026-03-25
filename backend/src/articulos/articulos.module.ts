5132f34ae482e392c7a798be1ec6c7b4d1af846e
Gael <gael.lopez22@uabc.edu.mx>
2026-03-24 18:48:29 -0700

    ligeros cambios

--- backend/src/articulos/articulos.module.ts
+++ backend/src/articulos/articulos.module.ts
@@ -1,32 +1,22 @@
 import { Module } from '@nestjs/common';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { Articulo } from './entities/articulo.entity';
-
 import { ArticuloDetalle, ArticuloDetalleSchema } from './schemas/articulo-detalle.schema';
-
+import { Revision, RevisionSchema } from '../asignaciones/schemas/revision.schema';
 import { ArticulosService } from './articulos.service';
-
-
+import { ArticulosController } from './articulos.controller';
 
 @Module({
-
   imports: [
-
     TypeOrmModule.forFeature([Articulo]),
-
-    MongooseModule.forFeature([{ name: ArticuloDetalle.name, schema: ArticuloDetalleSchema }]),
-
+    MongooseModule.forFeature([
+      { name: ArticuloDetalle.name, schema: ArticuloDetalleSchema },
+      { name: Revision.name, schema: RevisionSchema }
+    ]),
   ],
-
+  controllers: [ArticulosController],
   providers: [ArticulosService],
-
   exports: [ArticulosService],
-
 })
-
 export class ArticulosModule {}
-
