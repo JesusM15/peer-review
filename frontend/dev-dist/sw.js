fc3c30d27910f9be23af509763c5d757e25f5c4e
KitziaGarcia <kitzia.daniela3101@gmail.com>
2026-05-25 18:17:11 -0700

    v

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-eda88673'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oe1tihr92ho"
+    "revision": "0.cds6i1utle"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
